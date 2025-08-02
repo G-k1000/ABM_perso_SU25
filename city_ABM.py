d8a3dc077d5bc10777e16279d11d784c6355d852
Camille <cguibourge101@gmail.com>
2025-08-02 04:47:51 -0700

    city 3

--- city_ABM.py
+++ city_ABM.py
@@ -7,7 +7,8 @@ import matplotlib.pyplot as plt
 class Oil: 
     def __init__(self, base_price):
         self.price = base_price
-        self.demand = 0
+        self.demand = {}
+        self.demand_total = 0
         self.supply = float('inf')
 
     def receive_demand(self, firm_id, quantity):
@@ -21,10 +22,12 @@ class Gas:
     def __init__(self, base_price):
         self.price = base_price
         self.demand = {}
+        self.demand_total = 0
         self.supply = float('inf')
 
     def receive_demand(self, firm_id, quantity):
         self.demand[firm_id] = quantity
+
     
     def update_price(self):
         pass
@@ -33,7 +36,8 @@ class Gas:
 class Electricity:
     def __init__(self, base_price):
         self.price = base_price
-        self.demand = 0
+        self.demand = {}
+        self.demand_total = 0
         self.supply = float('inf')
 
     def receive_demand(self, firm_id, quantity):
@@ -76,7 +80,7 @@ class Firm(mesa.Agent):
 
     '''
     def __init__(self,
-                 model,
+                 model: mesa.Model,
                  markup: float,
                  lab_prod: float,
                  wage: float,
@@ -86,30 +90,52 @@ class Firm(mesa.Agent):
                  alpha: float,
                  ):
         super().__init__(model)
+        # energy demanded by firms
         self.oil_demand = 0
         self.gas_demand = 0
         self.electricity_demand = 0
+
+        # labor demanded and hired
+        self.labor_demand = 0
+        self.labor_hired = 0
+
+        # number of good per unit of X energy
         self.good_per_oil = good_per_oil
         self.good_per_gas = good_per_gas
         self.good_per_elec = good_per_elec
+
+        # energy cost of producing a single good
         self.energy_cost = self.model.oil.price + self.model.gas.price + self.model.electricity.price
-        self.output = 0
+        
+        # price setting parameter
         self.wage = wage
         self.markup = markup
         self.lab_prod = lab_prod
         self.price = (1 + self.markup)*(self.wage/self.lab_prod + self.energy_cost)
-        self.labor_demand = 0
+
+        # parameter of production function
         self.alpha = alpha
 
+        self.output = 0
+
     def production(self):
-        self.output = self.lab_prod * self.labor_demand ** self.alpha
+        '''
+        produce output given optimal amount of labor.
+        '''
+        self.output = np.floor(self.lab_prod * self.labor_demand ** self.alpha)
 
     def demand_labor(self):
-        self.labor_demand = (self.wage / (self.alpha * self.lab_prod * (self.price - self.energy_cost))) ** (1/(1 - self.alpha))
+        '''
+        determine optimal amount of labor by maximizing profit.
+        '''
+        self.labor_demand = np.floor(self.wage / (self.alpha * self.lab_prod * (self.price - self.energy_cost))) ** (1/(1 - self.alpha))
 
     def demand_energy(self):
+        '''
+        demand energy to supplier
+        '''
         # compute energy needed (linear function of quantity produced)
-        oil_quant = self.good_per_oil*self.outputs
+        oil_quant = self.good_per_oil*self.output
         self.model.oil.receive_demand(self.unique_id, oil_quant)
 
         gas_quant = self.good_per_gas*self.output
@@ -118,6 +144,20 @@ class Firm(mesa.Agent):
         elec_quant = self.good_per_elec*self.output
         self.model.electricity.receive_demand(self.unique_id, elec_quant)
 
+
+class Household(mesa.Agent):
+    def __init__(self, model, min_good):
+        # reservation wage
+        self.res_wage = min_good / self.model.firm.price
+
+        # wealth
+        self.wealth = 0
+
+
+class LaborMarket:
+    def __init__(self, model):
+
+    def match(self):
 
 
 
